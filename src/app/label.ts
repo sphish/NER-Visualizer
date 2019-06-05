cb533a8503dc832452761a2d3932b864f30e9f63
Shangyan Zhou <wulala979@outlook.com>
2019-06-06 02:49:29 +0800

    add_label

--- src/app/label.ts
+++ src/app/label.ts
@@ -6,6 +6,9 @@ export class Label {
 export const LABELS = [
   {name: 'LOC', selected: true},
   {name: 'PER', selected: true},
-  {name: 'MIS', selected: true},
   {name: 'ORG', selected: true},
+  {name: 'MIS', selected: true},
+  {name: 'FCP', selected: true},
+  {name: 'TME', selected: true},
+  {name: 'NUM', selected: true},
 ];
